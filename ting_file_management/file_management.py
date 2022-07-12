caee603bb526b345a3de9c7471996364846d38eb
Danilo <danilo.santossouza@outlook.com>
2022-07-12 17:55:36 -0300

    feat: cria funcao para ter arquivos txt

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,21 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    file = path_file.split(".")
+    file_read = list()
+
+    try:
+        if file[1] != 'txt':
+            return print("Formato inválido", file=sys.stderr)
+
+        file_open = open(path_file)
+        lines = file_open.readlines()
+
+        for line in lines:
+            treated_line = line.split('\n')
+            file_read.append(treated_line[0])
+    except:
+        return print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    else:
+        return file_read
