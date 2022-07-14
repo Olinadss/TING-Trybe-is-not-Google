e64de168780474e5ac512a52e1b751b076406212
Danilo <danilo.santossouza@outlook.com>
2022-07-14 17:04:12 -0300

    feat: cria funcao que verifica que existe a palavra

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    word_list = list()
+
+    for file_position in range(len(instance)):
+        number_of_occurrences = list()
+        file = instance.search(file_position)
+
+        for index, word_value in enumerate(file["linhas_do_arquivo"]):
+            if word.lower() in word_value.lower():
+                number_of_occurrences.append({"linha": index + 1})
+
+        if number_of_occurrences:
+            word_list.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": number_of_occurrences
+            })
+
+    return word_list
 
 
 def search_by_word(word, instance):
